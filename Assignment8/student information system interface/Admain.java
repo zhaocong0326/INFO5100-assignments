0569c2620c547c10a5bb150857f33d7a795ad6be
zhaocong0326 <46342826+zhaocong0326@users.noreply.github.com>
2019-11-13 17:07:23 -0800

    Update and rename StudentsManager.java to Admain.java

--- Assignment8/student information system interface/Admain.java
+++ Assignment8/student information system interface/Admain.java
@@ -3,28 +3,28 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class StudentsManager {
+public class Admain {
 	private Map<String, Student> map = new HashMap<String, Student>();
 
 	public void add(Student student) throws RuntimeException {
-		String studentId = student.getId();
-		if (map.get(studentId) == null) {
+		String id = student.getId();
+		Student s = map.get(id);
+		if (s == null) {
 			map.put(student.getId(), student);
-		} 
+		}
 	}
-
 
 	public void delete (String id) {
 		map.remove(id);
 	}
 
-
-	public int getStudentCount() {
-		return map.size();
+	public List<Student> getStudents() {
+		List<Student> students = new ArrayList<>(map.values());
+		return students;
 	}
 
-
-	public List<Student> getStudents() {
-		return new ArrayList<Student>(map.values());
+	public int getStudentNumber() {
+		int studentNumber = map.size();
+		return studentNumber;
 	}
 }
